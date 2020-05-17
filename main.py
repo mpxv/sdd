69fe6c3a6de4c5193da2692a6e8ec41ba90376c9
Alexey T <[omitted]>
2020-05-17 10:17:00 -0400

    fixed dumb stuff again now shows lines between people at safe distances too

--- main.py
+++ main.py
@@ -17,13 +17,13 @@ def draw_detections(img, rects, thickness = 1):
         pad_w, pad_h = int(0.15*w), int(0.05*h)
         cv2.rectangle(img, (x+pad_w, y+pad_h), (x+w-pad_w, y+h-pad_h), (0, 255, 0), thickness)
 
-def draw_line(img, p1, p2):
+def draw_line(img, p1, p2, text, color):
     xy1=tuple(p1[:2]+p1[2:]//2)
     xy2=tuple(p2[:2]+p2[2:]//2)
-    cv2.line(img, xy1, xy2, [0,0,255])
+    cv2.line(img, xy1, xy2, color)
     # Line thickness of 2 px 
     # breakpoint()
-    cv2.putText(img, 'too close', tuple(np.mean([xy1,xy2],axis=0,dtype=int)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, [255,255,255], 1) 
+    cv2.putText(img, text, tuple(np.mean([xy1,xy2],axis=0,dtype=int)+np.array([-30,-10])), cv2.FONT_HERSHEY_SIMPLEX, 0.5, [255,255,255], 1) 
 
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector( cv2.HOGDescriptor_getDefaultPeopleDetector() )
@@ -48,15 +48,16 @@ for group in output:
     # group[2] = person 1
     # group[3] = person 2
     distance = (constants.AVG_HEIGHT / group[1]) * group[0]
+    p1 = group[2]
+    p2 = group[3]
+
     if distance < 183: # if less than 183 cm or 6 feet apart
         print("Pair " + str(i) + ": Unsafe! Distance: " + str(round(distance)) + "cm")
-        p1 = group[2]
-        p2 = group[3]
         # breakpoint()
-        cv2.line(frame, tuple(p1[:2]+p1[2:]//2), tuple(p2[:2]+p2[2:]//2), [0,0,255])
-        draw_line(frame, p1, p2)
+        draw_line(frame, p1, p2, 'unsafe', [0,0,255])
     else:
         print("Pair " + str(i) + ": Safe! Distance: " + str(round(distance)) + "cm")
+        draw_line(frame, p1, p2, 'safe', [0,255,0])
     i += 1
 
 cv2.imshow('feedp',frame)
