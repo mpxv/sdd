7f4b15a56c896c04c943a9cdbd66ae5a6b37c2c6
owlman777 <62773526+owlman777@users.noreply.github.com>
2020-05-16 22:26:31 -0400

    fixed a stupid mistake

--- test.py
+++ test.py
@@ -18,14 +18,12 @@ def draw_detections(img, rects, thickness = 1):
 
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector( cv2.HOGDescriptor_getDefaultPeopleDetector() )
-# cap=cv2.VideoCapture('vid.avi')
-frame=cv2.imread('people.png')
+frame=cv2.imread('social-distancing-hero.png')
 
+found,w=hog.detectMultiScale(frame, winStride=(8,8), padding=(32,32), scale=1.05)
+draw_detections(frame,found)
+cv2.imshow('feed',frame)
 while True:
-    # _,frame=cap.read()
-    found,w=hog.detectMultiScale(frame, winStride=(8,8), padding=(32,32), scale=1.05)
-    draw_detections(frame,found)
-    cv2.imshow('feed',frame)
     ch = 0xFF & cv2.waitKey(1)
     if ch == 27:
         break
